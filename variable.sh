d13274cf9b70b734ca4fd4dad1f26687cce60e8a
Ubuntu <ubuntu@ip-172-31-25-2.us-west-2.compute.internal>
2026-06-08 06:23:36 +0000

    Added variables practice

---
+++ variable.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+
+# user defined variable
+
+Football="Sunil Chettri"
+
+Cricket="Virat Kohli"
+
+echo " $Football is Goat of Football"
+
+echo " $Cricket is Goat of Cricket"
+
+# pre defined variable(shell/environment variable)
+
+echo "what is my current login user= $USER"
+
+
+
