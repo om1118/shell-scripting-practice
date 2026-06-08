0e5ce834a6754d6620e827a0330f3ec62a2503d1
Ubuntu <ubuntu@ip-172-31-25-2.us-west-2.compute.internal>
2026-06-08 05:22:14 +0000

    Basic shell script commands

---
+++ om.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+echo "Good Morning"
+
+echo "Hi this is om here."
+
+echo "Here are some basic commads to practice"
+
+mkdir -p test
+
+echo " This is my first file created using shell script" > test.txt
+
+
