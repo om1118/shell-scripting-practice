fd59ad2b61a0969b4c33b9e973d2e91b1ea534eb
Ubuntu <ubuntu@ip-172-31-25-2.us-west-2.compute.internal>
2026-06-20 13:52:14 +0000

    loops

---
+++ 08-loops/loop.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+
+<<info
+
+loops : anthing that you want to repeatr again and again based on condition 
+
+info
+
+for (( num=1 ; num<=10 ; num++ ))
+do
+	echo "$num"
+	echo "hi their"
+done
+
+
